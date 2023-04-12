b03a8ccd79d09d1b5a0130d9ab5a779ac57d0b1e
zaxwebs <zifidigital@gmail.com>
2023-04-12 10:45:19 +0530

    updated fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,11 @@
     export let data;
 </script>
 
-<div class="container py-5">
-    <h1 class="mb-3">My Blog</h1>
-    <main>
-        <div>
-            {#each data.feed as post (post.id)}
-                <Post title={post.title} content={post.content} author={post.author} />
-            {/each}
-        </div>
-    </main>
-</div>
+<h1 class="mb-3">My Blog</h1>
+<main>
+    <div>
+        {#each data.feed as post (post.id)}
+            <Post title={post.title} content={post.content} author={post.author} />
+        {/each}
+    </div>
+</main>
